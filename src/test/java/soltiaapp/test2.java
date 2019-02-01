b08719938fd3193f491a7ac649d4001e0cbd207a
etxmsl <martin.landhage@msc.se>
2019-02-01 11:24:17 +0100

    junit 5.3.2

--- src/test/java/soltiaapp/test2.java
+++ src/test/java/soltiaapp/test2.java
@@ -1,60 +1,58 @@
 package soltiaapp;
 
-import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
+import org.junit.jupiter.api.*;
 import soltiaapp.se.IPageChrome;
 import soltiaapp.se.StartPage;
 
 public class test2 {
-	StartPage soltiaapp=new StartPage(15);
-	
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		   IPageChrome.startDriver();  
+	StartPage soltiaapp = new StartPage(15);
+
+	@BeforeAll
+	public static void setUpBeforeClass(StartPage soltiaapp) throws Exception {
+		IPageChrome.startDriver();
+		boolean loaded = soltiaapp.waitForVisabilitybyid("menu-item-448");
+		if (loaded) {
+			boolean app1 = soltiaapp.findElementByidandclick("menu-item-448");
+			if (app1) {
+				soltiaapp.waitForVisabilitybyid("psw");
+			}
+		}
+
 	}
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-		   IPageChrome.killDriver();
+	@AfterAll
+	public static void tearDownAfterClass(StartPage soltiaapp) throws Exception {
+		soltiaapp.loadpage();
+		IPageChrome.killDriver();
 	}
 
-	@Before
-	public void app1loaded (){
-		   boolean loaded=soltiaapp.waitForVisabilitybyid("menu-item-448");
-		   if (loaded){
-			   boolean app1=soltiaapp.findElementByidandclick("menu-item-448");
-			   if(app1){
-				  soltiaapp.waitForVisabilitybyid("psw");
-			   }
-		   }
-	}
-	
-	@After
-	public void reloadpage(){
-		   soltiaapp.loadpage();
-	}
-	
+	/*
+	 * @Before public void app1loaded (){ boolean
+	 * loaded=soltiaapp.waitForVisabilitybyid("menu-item-448"); if (loaded){ boolean
+	 * app1=soltiaapp.findElementByidandclick("menu-item-448"); if(app1){
+	 * soltiaapp.waitForVisabilitybyid("psw"); } } }
+	 */
+
+	/*
+	 * @After public void reloadpage(){ soltiaapp.loadpage(); }
+	 */
+
 	@Test
-	public void tc5_Login_user(){
-		   boolean userloggedin=false;
-		   soltiaapp.findInputByIdandsendkeys("email", "semala02");
-		   soltiaapp.findInputByIdandsendkeys("psw", "kleven");
-		   boolean login=soltiaapp.findElementBynameandclick("loginuser");
-		   if (login)	{  
-			   boolean scansloaded=soltiaapp.waitForVisabilitybyid("scans");
-			   boolean economysystemsloaded=soltiaapp.waitForVisabilitybyid("economysystems");
-			   if (scansloaded&&economysystemsloaded){
-						userloggedin=soltiaapp.findElementByidandclick("menu-item-451");
-					}
-					
-			   }
-		   assertTrue("User not logged in",userloggedin);
+	public void tc5_Login_user() {
+		boolean userloggedin = false;
+		soltiaapp.findInputByIdandsendkeys("email", "semala02");
+		soltiaapp.findInputByIdandsendkeys("psw", "kleven");
+		boolean login = soltiaapp.findElementBynameandclick("loginuser");
+		if (login) {
+			boolean scansloaded = soltiaapp.waitForVisabilitybyid("scans");
+			boolean economysystemsloaded = soltiaapp.waitForVisabilitybyid("economysystems");
+			if (scansloaded && economysystemsloaded) {
+				userloggedin = soltiaapp.findElementByidandclick("menu-item-451");
+			}
+
+		}
+		// Assertions.assertTrue(userloggedin, () -> "User not logged in");
+		Assertions.assertTrue(userloggedin, "User not logged in");
 	}
 
 }
